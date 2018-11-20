ae15914269093e111d2c5abe30390489d046768c
Georg <georg-black@hotmail.de>
2018-11-20 22:06:30 +0100

    App Icon hinzugefügt. App Name in "WeinRechnerApp" umbenannt. App Titel im Hamburger-Menü in "WeinRechnerApp" umbenannt.

--- Weinrechnerlel/Weinrechnerlel/Weinrechnerlel.Android/Properties/AssemblyInfo.cs
+++ Weinrechnerlel/Weinrechnerlel/Weinrechnerlel.Android/Properties/AssemblyInfo.cs
@@ -16,6 +16,8 @@ using Android.App;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 
+[assembly: Application(Icon = "@drawable/icon")]
+
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
