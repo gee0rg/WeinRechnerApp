7332da71a4a8d59b1ae2ede38af60ae13ef761e3
Tim Wirzius <timwirzius@googlemail.com>
2018-12-08 15:04:30 +0100

    bug beim eintragen der Flaschen Werte gefixt-------Berechnung schenit noch komisch zu sein

--- Weinrechnerlel/Weinrechnerlel/Weinrechnerlel/Ergebnis_Rechnung_Basis.xaml.cs
+++ Weinrechnerlel/Weinrechnerlel/Weinrechnerlel/Ergebnis_Rechnung_Basis.xaml.cs
@@ -21,18 +21,59 @@ namespace Weinrechnerlel
             liter_gw_prozent.Text = Convert.ToString(ergebnis.liter_gw_prozent);
             liter_vw_prozent.Text = Convert.ToString(ergebnis.liter_vw_prozent);
 
-           
+
         }
 
         private void verw_liter1_TextChanged(object sender, TextChangedEventArgs e)
         {
+            double liter1;
+            double liter075;
+            double liter05;
+            double liter0375;
+
+
+            try
+            {
+                 liter1 = Convert.ToDouble(verw_liter1.Text);
+            }
+            catch
+            {
+                 liter1 = 0;
+            }
+            try
+            {
+                 liter075 = Convert.ToDouble(verw_liter075.Text);
+            }
+            catch
+            {
+                 liter075 = 0;
+            }
+
+            try
+            {
+                 liter05= Convert.ToDouble(verw_liter05.Text);
+            }
+            catch
+            {
+                 liter05 = 0;
+            }
+            try
+            {
+                 liter0375= Convert.ToDouble(verw_liter0375.Text);
+            }
+            catch
+            {
+                 liter0375 = 0;
+            }
+
+         
             double rest_liter;
-            double a = Convert.ToDouble(liter_gw.Text) - (Convert.ToDouble(verw_liter1.Text) + Convert.ToDouble(verw_liter075.Text) * 0.75 + Convert.ToDouble(verw_liter05.Text) * 0.5 + Convert.ToDouble(verw_liter0375.Text) * 0.375);
-            rest_liter =  Math.Floor(a);
-            verw_liter1.Text = Convert.ToString(Math.Floor(rest_liter));
-            verw_liter075.Text = Convert.ToString(Math.Floor(rest_liter / 0.75));
-            verw_liter05.Text = Convert.ToString( Math.Floor(rest_liter / 0.5));
-            verw_liter0375.Text =Convert.ToString( Math.Floor(rest_liter / 0.375));
+            double a = Convert.ToDouble(liter_gw.Text) - liter1 + liter075 * 0.75 + liter05 * 0.5 + liter0375 * 0.375;
+            rest_liter = Math.Floor(a);
+            verw_liter1_erg.Text = Convert.ToString(Math.Floor(rest_liter));
+            verw_liter075_erg.Text = Convert.ToString(Math.Floor(rest_liter / 0.75));
+            verw_liter05_erg.Text = Convert.ToString(Math.Floor(rest_liter / 0.5));
+            verw_liter0375_erg.Text = Convert.ToString(Math.Floor(rest_liter / 0.375));
         }
 
 
