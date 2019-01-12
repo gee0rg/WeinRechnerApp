8c7f276f75cdfddc5483c2753e677fb6d77c5727
Tim Wirzius <timwirzius@googlemail.com>
2019-01-12 19:21:00 +0100

    bug fixes

--- Weinrechnerlel/Weinrechnerlel/Weinrechnerlel/App.xaml.cs
+++ Weinrechnerlel/Weinrechnerlel/Weinrechnerlel/App.xaml.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using Weinrechnerlel.ViedwModels;
+
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
--- Weinrechnerlel/Weinrechnerlel/Weinrechnerlel/Startpage.xaml.cs
+++ Weinrechnerlel/Weinrechnerlel/Weinrechnerlel/Startpage.xaml.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Weinrechnerlel.ViedwModels;
+
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Weinrechnerlel.Models;
 using Newtonsoft.Json;
-using Weinrechnerlel.Helper;
+
 
 namespace Weinrechnerlel
 {
